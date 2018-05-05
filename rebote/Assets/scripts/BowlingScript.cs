c998e3dec48bf0b9cc29b154ee6d86b6ebbdb094
Alumno10 j401 <a10j401@campustls.local>
2018-05-05 15:44:06 -0500

    ejercicio en clase y tarea de la pelota de playa con rebote y botones en teclado

---
+++ rebote/Assets/scripts/BowlingScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingScript : MonoBehaviour {
+
+	public Rigidbody2D ballBody;
+
+	private float forceX = 08.0f;
+	private float forceY = 12.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			Vector2 forceLeft = new Vector2 (-forceX, 0);
+			ballBody.AddForce(forceLeft);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			Vector2 forceRight = new Vector2 (forceX, 0);
+			ballBody.AddForce(forceRight);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			Vector2 forceUp = new Vector2 (0,forceY);
+			ballBody.AddForce(forceUp);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			Vector2 forceDown = new Vector2 (0,-forceY);
+			ballBody.AddForce(forceDown);
+		}
+	}
+}
