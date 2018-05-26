4e730b0f7b622d3f803169b75293bafc2eb92b96
Alumno10 j401 <a10j401@campustls.local>
2018-05-26 15:28:31 -0500

    ejercicio con monedas

---
+++ proyecto2/Assets/scripts/ControlMonedas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ControlMonedas : MonoBehaviour {
+
+    public Text textoMonedas;
+    private int contadorMonedas;
+
+	// Use this for initialization
+	void Start () {
+        contadorMonedas = 0;
+
+    }
+	
+	// Update is called once per frame
+	/*void Update () {
+		
+	}*/
+
+    public void AumentarMonedas()
+    {
+        contadorMonedas++;
+        textoMonedas.text = "Monedas: " + contadorMonedas;
+    }
+}
---
+++ proyecto2/Assets/scripts/pollitoScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pollitoScript : MonoBehaviour {
+
+    private Rigidbody2D fisicaPollito;
+    public ControlMonedas codigoMonedas;
+
+	// Use this for initialization
+	void Start () {
+        fisicaPollito = GetComponent<Rigidbody2D>(); //esto hace que si el pollito tiene fisica, que lo guarde en el inicio
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector2 fuerzaIzquierda = new Vector2(-10, 0);
+            fisicaPollito.AddForce(fuerzaIzquierda);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector2 fuerzaDerecha = new Vector2(10, 0);
+            fisicaPollito.AddForce(fuerzaDerecha);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector2 fuerzaAbajo = new Vector2(0, -10);
+            fisicaPollito.AddForce(fuerzaAbajo);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector2 fuerzaArriba = new Vector2(0, 10);
+            fisicaPollito.AddForce(fuerzaArriba);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Coin")
+        {
+            collision.gameObject.SetActive(false);
+            codigoMonedas.AumentarMonedas();
+        }
+    }
+}
